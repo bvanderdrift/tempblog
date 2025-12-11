1b65864ec1d39601f0ac8852ee1747503a7924e1
Beer van der Drift <beerdrift@gmail.com>
2025-12-11 12:27:10 -0700

    feat: add posts schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,12 @@
 import { authTables } from '@convex-dev/auth/server'
-import { defineSchema } from 'convex/server'
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
 
 export default defineSchema({
   ...authTables,
+  posts: defineTable({
+    body: v.string(),
+    authorId: v.id('users'),
+    publishedAt: v.union(v.number(), v.null()),
+  }).index('by_user', ['authorId']),
 })
